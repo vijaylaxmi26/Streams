4e345ad01f7861403c72f60b75222f6fb1643d4a
vijaylaxmi26 <68533681+vijaylaxmi26@users.noreply.github.com>
2021-07-05 19:12:57 +0530

    update in stream form

--- client/src/components/streams/StreamForm.js
+++ client/src/components/streams/StreamForm.js
@@ -1,5 +1,69 @@
+import React from "react";
+import { Form, Field } from "react-final-form";
+ 
+const StreamForm = (props) => {
+  const renderError = ({ error, touched }) => {
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      );
+    }
+  };
+ 
+  const renderInput = ({ input, label, meta }) => {
+    const className = `field ${meta.error && meta.touched ? "error" : ""}`;
+    return (
+      <div className={className}>
+        <label>{label}</label>
+        <input {...input} autoComplete="off" />
+        {renderError(meta)}
+      </div>
+    );
+  };
+ 
+  const onSubmit = (formValues) => {
+    props.onSubmit(formValues);
+  };
+ 
+  return (
+    <Form
+      initialValues={props.initialValues}
+      onSubmit={onSubmit}
+      validate={(formValues) => {
+        const errors = {};
+ 
+        if (!formValues.title) {
+          errors.title = "You must enter a title";
+        }
+ 
+        if (!formValues.description) {
+          errors.description = "You must enter a description";
+        }
+ 
+        return errors;
+      }}
+      render={({ handleSubmit }) => (
+        <form onSubmit={handleSubmit} className="ui form error">
+          <Field name="title" component={renderInput} label="Enter Title" />
+          <Field
+            name="description"
+            component={renderInput}
+            label="Enter Description"
+          />
+          <button className="ui button primary">Submit</button>
+        </form>
+      )}
+    />
+  );
+};
+ 
+export default StreamForm;
+
+/*
 import React from 'react';
-import {Field, reduxForm } from 'redux-form';
+import {Field,reduxForm } from 'redux-form';
 
 class StreamForm extends React.Component{
 
@@ -13,7 +77,7 @@ class StreamForm extends React.Component{
                 </div>
             );
         }
-    }
+    };
              //formProps
     renderInput = ({input, label, meta} ) => {
         const classN = `field ${meta.error && meta.touched ? 'error' : ''}`;
@@ -26,13 +90,13 @@ class StreamForm extends React.Component{
             </div>
         );
 
-        /*(<input 
-        onChange={formProps.input.onChange} 
-        value={formProps.input.value}
-        />);
-        */
+        // (<input 
+        // onChange={formProps.input.onChange} 
+        // value={formProps.input.value}
+        // />);
+        
         //<input {...formProps.input} />
-    }
+    };
 
 
     onSubmit = (formValues) => {
@@ -77,4 +141,4 @@ export default reduxForm({
     validate,
 })(StreamForm);
 
- 
+ */
